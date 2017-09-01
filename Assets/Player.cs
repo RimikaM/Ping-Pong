febb6fe8cd732b403cc492d32dff89d7fb5f9337
RimikaM <rimikamajumdar@Rimikas-MacBook-Pro.local>
2017-08-31 22:53:11 -0700

    need to fix problem of player being able to move out of arena

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ePlayer {
+	Left,
+	Right
+}
+
+public class Player : MonoBehaviour {
+
+	// units the player moves per second
+	public float speed = 15f;
+	public ePlayer player;
+
+	// check if player pressed a key to decide to move the player
+	void FixedUpdate () {
+		float inputSpeed = 0f;
+		if (player == ePlayer.Left) {
+			inputSpeed = Input.GetAxisRaw ("PlayerLeft");
+		} else if (player == ePlayer.Right) {
+			inputSpeed = Input.GetAxisRaw ("PlayerRight");
+		}
+		transform.position += new Vector3 (0f, 0f, inputSpeed * speed * Time.deltaTime);
+
+	}
+}
