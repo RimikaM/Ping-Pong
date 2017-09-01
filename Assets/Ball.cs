5493380ff62e1196482fb5dc1e99536e923e4c37
RimikaM <rimikamajumdar@Rimikas-MacBook-Pro.local>
2017-08-31 22:25:25 -0700

    everything looks good, ball can move around, need the players to move

---
+++ Assets/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	public Vector3 initialImpulse;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce(initialImpulse, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
